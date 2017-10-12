a0c0b108234eeba0edabc717716d151db936e9d1
Bobby Huang <zhenghuahuang@gmail.com>
2017-10-12 15:25:15 -0400

    added back testing slack token

--- libs/slackTokenVerification.js
+++ libs/slackTokenVerification.js
@@ -1,7 +1,8 @@
 const slackToken = process.env.SLACK_TOKEN
+const testingSlackToken = 'zXvD5nqzIdDweibfwCAtSCPL'
 
 module.exports = function slackVerification(req, res, next) {
-  if (req.body.token === slackToken) {
+  if (req.body.token === slackToken || req.body.token === testingSlackToken) {
     req.body.requestTime = new Date()
     next()
   } else {
