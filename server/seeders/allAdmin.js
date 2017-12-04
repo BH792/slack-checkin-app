c449cc12ba1a5d5c0bd89dc13a6a38d10653636d
Bobby Huang <zhenghuahuang@gmail.com>
2017-12-04 11:42:44 -0500

    removed slack token

--- server/seeders/allAdmin.js
+++ server/seeders/allAdmin.js
@@ -2,7 +2,6 @@ const slackWebAPIAdapter = require('../../libs/slackWebAPIAdapter')
 const slackAdapter = new slackWebAPIAdapter()
 const { Admin } = require('../models')
 
-slackAdapter.token = 'xoxp-2727337933-219362225906-257309535558-d927b8ae0eae6b881e92264dac16e9ac'
 slackAdapter.get('users.list', {})
   .then(json => {
     let adminMembers = json.members.filter(member => member.is_admin === true)
